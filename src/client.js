4c001d821b0bfb1f5cebf5f2e214e23c8025072e
stepan litvinov <stepan_litvinov@epam.com>
2019-01-06 17:58:08 +0300

    fix for ssr

--- src/client.js
+++ src/client.js
@@ -2,9 +2,24 @@ import React from 'react'
 import {hydrate} from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import App from './app/App'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import purple from '@material-ui/core/colors/purple'
+import {createGenerateClassName, createMuiTheme, MuiThemeProvider,} from '@material-ui/core/styles';
+import {JssProvider} from 'react-jss'
+
+class Main extends React.Component {
+    // Remove the server-side injected CSS.
+    componentDidMount() {
+        const jssStyles = document.getElementById('jss-server-side');
+        if (jssStyles && jssStyles.parentNode) {
+            jssStyles.parentNode.removeChild(jssStyles);
+        }
+    }
+
+    render() {
+        return <App/>
+    }
+}
+
 
 // Тема на клиенте должна быть такой же, как и на сервере
 // При желании можно даже вынести в отдельный модуль
@@ -20,12 +35,17 @@ const theme = createMuiTheme({
     },
 })
 
+// Create a new class name generator.
+const generateClassName = createGenerateClassName();
+
 // Оборачиваем приложение созданной темой
 hydrate(
-    <MuiThemeProvider theme={theme}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </MuiThemeProvider>,
+    <BrowserRouter>
+        <JssProvider generateClassName={generateClassName}>
+            <MuiThemeProvider theme={theme}>
+                <Main/>
+            </MuiThemeProvider>
+        </JssProvider>
+    </BrowserRouter>,
     document.querySelector('#app')
 )
