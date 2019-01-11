e2a1cd08f2423e1baa8481be9703ecd9399dd3f5
stepan litvinov <stepan_litvinov@epam.com>
2019-01-11 15:50:22 +0300

    refactoring

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,6 @@ scp docker-compose.yml 192.168.1.20:~/
 ssh 192.168.1.20 '
 docker-compose down
 docker-compose pull
-docker-compose -p app up -d
+docker-compose up -d
 '
 open -a /Applications/Safari.app https://selector.site
